141a73c327bd10a4ba5b28644e011c896fe093c8
Jose Enrique Camacho Silvestre <enrique_silvestre28@outlook.com>
2024-06-18 22:33:40 -0400

    jc/ambi: Implementada funcionalidad para enviar correos.

---
+++ src/resources/views/mails/enviarCorreo.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Solicitud aprobada</title>
+</head>
+<body>
+    <h1>Solicitud de Reserva de ambiente aprobada</h1>
+    <p>Este es un correo generado automáticamente con los detalles de la solicitud de reserva.</p>
+</body>
+</html>
